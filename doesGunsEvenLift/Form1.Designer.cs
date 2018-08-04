0f39fe9113e9a0cd5071b869ed7cc8e36a04ff1c
En Emerson <007emersone@gmail.com>
2018-08-04 17:54:23 -0500

    add project files

---
+++ doesGunsEvenLift/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace doesGunsEvenLift
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectFileButton = new System.Windows.Forms.Button();
+            this.filePathBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.numOfLiftsBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.avgOfLiftsBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.threshLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // selectFileButton
+            // 
+            this.selectFileButton.Location = new System.Drawing.Point(12, 12);
+            this.selectFileButton.Name = "selectFileButton";
+            this.selectFileButton.Size = new System.Drawing.Size(75, 23);
+            this.selectFileButton.TabIndex = 0;
+            this.selectFileButton.Text = "Select File";
+            this.selectFileButton.UseVisualStyleBackColor = true;
+            this.selectFileButton.Click += new System.EventHandler(this.selectFileButton_Click);
+            // 
+            // filePathBox
+            // 
+            this.filePathBox.Location = new System.Drawing.Point(123, 14);
+            this.filePathBox.Name = "filePathBox";
+            this.filePathBox.ReadOnly = true;
+            this.filePathBox.Size = new System.Drawing.Size(257, 20);
+            this.filePathBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(205, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Result:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(251, 162);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.ReadOnly = true;
+            this.resultBox.Size = new System.Drawing.Size(129, 20);
+            this.resultBox.TabIndex = 3;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(26, 63);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(114, 75);
+            this.calcButton.TabIndex = 4;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // numOfLiftsBox
+            // 
+            this.numOfLiftsBox.Location = new System.Drawing.Point(251, 60);
+            this.numOfLiftsBox.Name = "numOfLiftsBox";
+            this.numOfLiftsBox.ReadOnly = true;
+            this.numOfLiftsBox.Size = new System.Drawing.Size(129, 20);
+            this.numOfLiftsBox.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Total Lifts:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // avgOfLiftsBox
+            // 
+            this.avgOfLiftsBox.Location = new System.Drawing.Point(251, 110);
+            this.avgOfLiftsBox.Name = "avgOfLiftsBox";
+            this.avgOfLiftsBox.ReadOnly = true;
+            this.avgOfLiftsBox.Size = new System.Drawing.Size(129, 20);
+            this.avgOfLiftsBox.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(173, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Average Lifts:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // threshLabel
+            // 
+            this.threshLabel.AutoSize = true;
+            this.threshLabel.Location = new System.Drawing.Point(12, 169);
+            this.threshLabel.Name = "threshLabel";
+            this.threshLabel.Size = new System.Drawing.Size(50, 13);
+            this.threshLabel.TabIndex = 9;
+            this.threshLabel.Text = "threshold";
+            this.threshLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 194);
+            this.Controls.Add(this.threshLabel);
+            this.Controls.Add(this.avgOfLiftsBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numOfLiftsBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filePathBox);
+            this.Controls.Add(this.selectFileButton);
+            this.Name = "Form1";
+            this.Text = "Does \'The Guns\' Even Lift?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectFileButton;
+        private System.Windows.Forms.TextBox filePathBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.TextBox numOfLiftsBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox avgOfLiftsBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label threshLabel;
+
+    }
+}
+
---
+++ doesGunsEvenLift/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace doesGunsEvenLift
+{
+    public partial class Form1 : Form
+    {
+        Stream myStream = null;
+        string directory = "No file selected.";
+        List<Double> liftRepsList = new List<Double>();
+        int liftThreshold = 4500;
+
+        public Form1()
+        {
+            InitializeComponent();
+            filePathBox.Text = directory;
+            threshLabel.Text = "Lift Threshold: " + liftThreshold.ToString();
+        }
+
+        private void selectFileButton_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            directory = openFileDialog1.FileName.ToString();
+                            getLifts();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+        public void getLifts()
+        {
+            StreamReader sr = new StreamReader(directory);
+            filePathBox.Text = directory.ToString();
+
+            try
+            {
+                double current;
+                string foo;
+                while ((foo = sr.ReadLine()) != null)
+                {
+                    current = Convert.ToDouble(foo);
+                    liftRepsList.Add(current);
+                    Console.WriteLine(current);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            if (directory != "No file selected.")
+            {
+                numOfLiftsBox.Text = liftRepsList.Count().ToString(); // number of lifts in the set
+                
+                double divisor = 0;
+                double dividend = liftRepsList.Count();
+                int avgLifts = 0;
+                int count = liftRepsList.Count();
+
+                for (int i = 0; i < count; i++)
+                {
+                    divisor += liftRepsList.ElementAt(i);
+                }
+
+                avgLifts = (int)(divisor / dividend);
+
+                avgOfLiftsBox.Text = avgLifts.ToString(); // average number of lifts
+
+                if (avgLifts < liftThreshold) //result of lift or not to lift
+                {
+                    resultBox.Text = "'The Guns' doesn't lift.";
+                }
+                else
+                {
+                    resultBox.Text = "'The Guns' lifts.";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a directory.");
+                filePathBox.Text = "Please select a directory";
+                numOfLiftsBox.Clear();
+                avgOfLiftsBox.Clear();
+                resultBox.Clear();
+            }
+        }
+    }
+}
